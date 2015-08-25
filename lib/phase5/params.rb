61f10cf72f988449163712c459fa27a1204bb184
Mikey Shean <mdshean2@gmail.com>
2015-08-25 17:50:10 -0400

    Complete through Phase 6.

--- lib/phase5/params.rb
+++ lib/phase5/params.rb
@@ -1,4 +1,5 @@
 require 'uri'
+require 'byebug'
 
 module Phase5
   class Params
@@ -10,9 +11,14 @@ module Phase5
     # You haven't done routing yet; but assume route params will be
     # passed in as a hash to `Params.new` as below:
     def initialize(req, route_params = {})
+      @req = req
+      @params = route_params
+      parse_www_encoded_form(@req.query_string)
+      parse_www_encoded_form(@req.body)
     end
 
     def [](key)
+      @params[key.to_sym] || @params[key.to_s]
     end
 
     # this will be useful if we want to `puts params` in the server log
@@ -29,11 +35,27 @@ module Phase5
     # should return
     # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
     def parse_www_encoded_form(www_encoded_form)
+      return if www_encoded_form.nil?
+      hash_values = URI::decode_www_form(www_encoded_form)
+      hash_values.each do |pair|
+        populate_params(parse_key(pair[0]), pair[1])
+      end
+    end
+
+    def populate_params(keys, value)
+      keys.inject(@params) do |hash, key|
+        if key == keys.last
+          hash[key] = value
+        else
+          hash[key] ||= {}
+        end
+      end
     end
 
     # this should return an array
     # user[address][street] should return ['user', 'address', 'street']
     def parse_key(key)
+      keys = key.split(/\]\[|\[|\]/)
     end
   end
 end
--- lib/phase6/router.rb
+++ lib/phase6/router.rb
@@ -1,17 +1,28 @@
+require 'byebug'
+
 module Phase6
   class Route
     attr_reader :pattern, :http_method, :controller_class, :action_name
 
     def initialize(pattern, http_method, controller_class, action_name)
+      @pattern, @http_method, @controller_class, @action_name =
+        pattern, http_method, controller_class, action_name
     end
 
     # checks if pattern matches path and method matches request method
     def matches?(req)
+      # byebug
+      pattern.match(req.path) && http_method == req.request_method
     end
 
     # use pattern to pull out route params (save for later?)
     # instantiate controller and call controller action
     def run(req, res)
+      route_params = {}
+      match_data = pattern.match(req.path)
+      route_params[:id] = match_data["id"] if match_data.names.include?("id")
+
+      controller_class.new(req, res, route_params).invoke_action(action_name)
     end
   end
 
@@ -19,10 +30,12 @@ module Phase6
     attr_reader :routes
 
     def initialize
+      @routes = []
     end
 
     # simply adds a new route to the list of routes
     def add_route(pattern, method, controller_class, action_name)
+      routes << Route.new(pattern, method, controller_class, action_name)
     end
 
     # evaluate the proc in the context of the instance
@@ -33,14 +46,20 @@ module Phase6
     # make each of these methods that
     # when called add route
     [:get, :post, :put, :delete].each do |http_method|
+      define_method(http_method) do |pattern, controller_class, action_name|
+        self.add_route(pattern, http_method, controller_class, action_name)
+      end
     end
 
     # should return the route that matches this request
     def match(req)
+      routes.find { |route| route.matches?(req) }
     end
 
     # either throw 404 or call run on a matched route
     def run(req, res)
+      route = Router.new.match(req)
+      route ? route.run : res.status = 404
     end
   end
 end
