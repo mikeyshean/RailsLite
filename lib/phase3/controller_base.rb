76514e64461fc9aed0f6020cf1ac4df95bde7008
Mikey Shean <mdshean2@gmail.com>
2015-08-25 14:50:46 -0400

    Complete through Phase 4.

--- lib/phase3/controller_base.rb
+++ lib/phase3/controller_base.rb
@@ -2,12 +2,18 @@ require_relative '../phase2/controller_base'
 require 'active_support'
 require 'active_support/core_ext'
 require 'erb'
+require 'active_support/inflector'
+require 'byebug'
 
 module Phase3
   class ControllerBase < Phase2::ControllerBase
     # use ERB and binding to evaluate templates
     # pass the rendered html to render_content
     def render(template_name)
+      file_path = "views/#{self.class.name.underscore}/#{template_name}.html.erb"
+      file = File.read(file_path)
+      
+      render_content(ERB.new(file).result(binding), "text/html")
     end
   end
 end
--- lib/phase4/session.rb
+++ lib/phase4/session.rb
@@ -1,22 +1,38 @@
 require 'json'
 require 'webrick'
+require 'byebug'
 
 module Phase4
   class Session
     # find the cookie for this app
     # deserialize the cookie into a hash
     def initialize(req)
+      @req = req
+      parse
+    end
+
+    def parse
+      @value = {}
+
+      @req.cookies.each do |cookie|
+        @value = JSON.parse(cookie.value) if cookie.name == "_rails_lite_app"
+      end
+      @value
     end
 
     def [](key)
+      @value[key]
     end
 
     def []=(key, val)
+      @value[key] = val
     end
 
     # serialize the hash into json and save in a cookie
     # add to the responses cookies
     def store_session(res)
+      res.cookies << WEBrick::Cookie.new("_rails_lite_app", @value.to_json)
+      self
     end
   end
 end
